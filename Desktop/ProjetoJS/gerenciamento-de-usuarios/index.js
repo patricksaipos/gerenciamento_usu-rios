90a02b88ef54660704fe481517736975e81dbcbf
Patrick Andrade <patrick.andrade@saipos.com>
2025-06-22 22:28:50 -0300

    G7 - Criando objeto a partir dos dados enviados no formulário

--- Desktop/ProjetoJS/gerenciamento-de-usuarios/index.js
+++ Desktop/ProjetoJS/gerenciamento-de-usuarios/index.js
@@ -1,10 +1,14 @@
 let fields = document.querySelectorAll('#form-user-create [name]');
+let user = {};
+
 fields.forEach((field, index) => {
-    if (field.name == 'gender' && field.checked) {
-
-        console.log('field: ', field)
-
+    if (field.name == "gender" && field.checked === false) {
+        console.log(field.value)
+        user.gender
     } else {
-
+        user[field.name] = field.value;
+        console.log(field.value)
     }
 })
+
+console.log(user)
