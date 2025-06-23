d1916dad0c645dffe308f5c509bd5296f842ee7b
Patrick Andrade <patrick.andrade@saipos.com>
2025-06-22 23:03:33 -0300

    G8 - Trabalhando com evento de submit no formulário e incluindo função para chamar o toast ao enviar o formulário

--- Desktop/ProjetoJS/gerenciamento-de-usuarios/index.js
+++ Desktop/ProjetoJS/gerenciamento-de-usuarios/index.js
@@ -1,14 +1,37 @@
 let fields = document.querySelectorAll('#form-user-create [name]');
 let user = {};
 
-fields.forEach((field, index) => {
-    if (field.name == "gender" && field.checked === false) {
-        console.log(field.value)
-        user.gender
-    } else {
-        user[field.name] = field.value;
-        console.log(field.value)
-    }
-})
+function toast(position, icon, title){
 
-console.log(user)
+    const Toast = Swal.mixin({
+        toast: true,
+        position: position,
+        showConfirmButton: false,
+        timer: 1500,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+          toast.onmouseenter = Swal.stopTimer;
+          toast.onmouseleave = Swal.resumeTimer;
+        }
+      });
+      Toast.fire({
+        icon: icon,
+        title: title
+      });
+
+}
+
+document.getElementById("form-user-create").addEventListener("submit", event => {
+    event.preventDefault();
+    fields.forEach((field, index) => {
+        if (field.name == "gender" && field.checked === false) {
+            user.gender
+        } else {
+            user[field.name] = field.value;
+        }
+    })
+
+    toast("top-end", "success", "Usuário Criado com Sucesso")
+
+    console.log(user)
+})
