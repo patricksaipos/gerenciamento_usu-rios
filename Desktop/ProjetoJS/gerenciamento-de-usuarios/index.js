5050b97988d316aef0a3bf7a9c41b8b09331bdae
Patrick Andrade <patrick.andrade@saipos.com>
2025-06-22 21:37:14 -0300

    Primeiro Commit

---
+++ Desktop/ProjetoJS/gerenciamento-de-usuarios/index.js
@@ -0,0 +1,9 @@
+let name = document.querySelector('#exampleInputName');
+let gender = document.querySelectorAll('#form-user-create [name=gender]:checked');
+let birth = document.querySelector('#exampleInputBirth');
+let country = document.querySelector('#exampleInputCountry');
+let email = document.querySelector('#exampleInputEmail');
+let password = document.querySelector('#exampleInputPassword');
+let photo = document.querySelector('#exampleInputPhoto');
+let admin = document.querySelector('#exampleInputAdmin');
+
